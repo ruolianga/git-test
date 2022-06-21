41aeac41c78a681aae6df3f106f79922b2b4a1a5
yg <yg@sina.cn>
2022-06-21 21:09:02 +0800

    second commit!

--- src/main/java/com/zpark/git/GitTest.java
+++ src/main/java/com/zpark/git/GitTest.java
@@ -3,5 +3,6 @@ package com.zpark.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("I love git! --master");
+        System.out.println("I love git! --master1");
     }
 }
