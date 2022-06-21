1021227804501e25bb9318ec04f5018f92290b8f
yg <yg@sina.cn>
2022-06-21 21:14:20 +0800

    hot-fix hb second commit!

--- src/main/java/com/zpark/git/GitTest.java
+++ src/main/java/com/zpark/git/GitTest.java
@@ -3,6 +3,6 @@ package com.zpark.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("I hate git! --hot-fix");
-
+        System.out.println("I hate git! --hot-fix1");
     }
 }
