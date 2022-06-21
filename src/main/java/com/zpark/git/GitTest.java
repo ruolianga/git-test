268be6035786f526f2d352e59bf5d881f98a3d1c
yg <yg@sina.cn>
2022-06-21 21:10:00 +0800

    hot-fix hb commit!

--- src/main/java/com/zpark/git/GitTest.java
+++ src/main/java/com/zpark/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("I love git! --master");
         System.out.println("I love git! --master1");
+        System.out.println("I hate git! --hot-fix");
     }
 }
